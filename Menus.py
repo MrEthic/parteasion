ab8ae034b1cdef81c1d7939d5ac8c5f66244dc79
Laurie <laurie.galmiche@isep.fr>
2019-04-18 15:54:24 +0200

    def ajouter

--- Menus.py
+++ Menus.py
@@ -1,5 +1,5 @@
 import Aliases as als
-import Partissions as prt
+import Utils as util
 
 def init():
     rtn = ' ' + 33*'/' + '|\n' + 2* ('||' + 30*' ' + '|||\n')
@@ -29,7 +29,7 @@ def home(datas):
     respond = getAnswer(['ajouter', 'supprimer', 'gere', 'sauvegarder', 'quitter'])
     actions.get(respond)(datas)
 
-def ajouter(datas):
+
     
 
 def ajouterPartition(datas):
@@ -74,6 +74,44 @@ def ajouterPartition(datas):
 
 def rechercher():
     print('')
+
+def ajouter():
+    print("que voulez-vous ajouter ? ","\n" , "1-partition","\n" , "2-compositceur" ,"\n" ,"3-editeur","\n" ,"4-instrument","\n" ,"5-format"))
+    type=getAnswer("partition","compositeur","editeur","instrument","format")
+
+    if type == "partition":
+        partition=util.createElement(MODEL_PARTITION)
+        numero="P"+ Datas.lastP
+        Datas.partitions[numero]=partition
+        Datas.lastP=Datas.lastP+1
+
+    if type == "compositeur":
+        compositeur=util.createElement(MODEL_COMPOSITEUR)
+        numero="C"+ Datas.lastC
+        Datas.compositeurs[numero]=compositeur
+        Datas.lastC=Datas.lastC+1
+
+    if type == "editeur":
+        editeur=util.createElement(MODEL_EDITEUR)
+        numero="E"+ Datas.lastE
+        Datas.editeurs[numero]=editeur
+        Datas.lastE=Datas.lastE+1
+
+    if type == "instrument":
+        instrument=util.createElement(MODEL_INSTRUMENT)
+        numero="P"+ Datas.lastI
+        Datas.instruments[numero]=instrument
+        Datas.lastI=Datas.lastI+1
+
+    if type == "format":
+        format=util.createElement(MODEL_FORMAT)
+        numero="P"+ Datas.lastF
+        Datas.formats[numero]=format
+        Datas.lastF=Datas.lastF+1
+
+#"blablabla".higher ---------> BLABLABLA
+#"BLABLABLA".lower - --------> blablabla
+
 
 
 actions = {
