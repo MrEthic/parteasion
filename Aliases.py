e20bdd13990740173630954cb70a3ab3e2f8713e
Jeremie <jeremiebasso@hotmail.fr>
2019-04-22 15:09:58 +0200

    mini modif

--- Aliases.py
+++ Aliases.py
@@ -1,27 +1,22 @@
 
 aliases = {
-    'Ajouter':['Ajouter', 'ajouter', 'ajouté', 'ajoute', 'ajout', 'add'],
-    'Rechercher':['Rechercher', 'rechercher', 'RECHERCHER', 'search', 'Search', 'SEARCH', 'rech', 'recherche', 'Recherche', 're', 'research'],
-    'Supprimer':['Supprimer', 'supprimer', 'SUPPRIMER', 'delete', 'supp', 'del', 'remove', 'enlever', 'supprim', 'suprimer'],
-    'Gérer':['Gérer', 'gerer', 'gere', 'gérer', 'manage'],
-    'Sauvegarder':['Sauvegarder', 'sauvegarder', 'Save', 'save', 'SAVE', 'sauvgarde', 'sauv'],
-    'Quitter':['Quitter', 'quitter', 'Quit', 'QUIT', 'Escape', 'ESCAPE', 'ESC', 'esc', 'quiter', 'kiter', 'qiter', 'kit'],
-    'Partition':['Partition', 'PART', 'partition', 'part', 'parti', 'partoche', 'Particion', 'Partission', 'PARTICION', 'PARTISSION'],
-    'Compositeur':['compositeur', 'Compositeur', 'Composer', 'COMPOSER', 'Comp', 'COMP', 'conpositeur', 'conpo'],
-    'Instrument':['Instrument', 'instrument', 'instrumant', 'instru', 'instrumen', 'instruman'],
-    'Enregistrement':['Enregistrement', 'enregistrement', 'RECORD', 'record', 'REC', 'Rec', 'Record', 'Enreg', 'enregistremant'],
-    'Editeur':['Editeur', 'editeur', 'edit', 'EDIT', 'editeure', 'editere', 'editeurre', 'editer'],
-    'Oui':['Oui', 'oui', 'Yes', 'Si', 'Y', 'OUI', 'ui', 'UI'],
-    'Non':['Non', 'non', 'No', 'N', 'NON', 'NEIN', 'nein',],
-    'Modifier':['Modifier', 'modifier', 'mod', 'MOD', 'change', 'modif', 'modife', 'modifer'],
-    'Annuler':['Annuler', 'annuler', 'Cancel', 'cancel', 'CANCEL', 'Annul', 'anuler', 'Anuler'],
-    'Format':['Format', 'format', 'forme', 'form', 'size', 'Size', 'taille', 'TAILLE', 'Taille']
+    'ajouter':['Ajouter', 'ajouter', 'ajouté', 'ajoute', 'ajout', 'add'],
+    'rechercher':['Rechercher', 'rechercher', 'RECHERCHER', 'search', 'Search', 'SEARCH', 'rech', 'recherche', 'Recherche', 're', 'research'],
+    'supprimer':['Supprimer', 'supprimer', 'SUPPRIMER', 'delete', 'supp', 'del', 'remove', 'enlever', 'supprim', 'suprimer'],
+    'gérer':['Gérer', 'gerer', 'gere', 'gérer', 'manage'],
+    'sauvegarder':['Sauvegarder', 'sauvegarder', 'Save', 'save', 'SAVE', 'sauvgarde', 'sauv'],
+    'quitter':['Quitter', 'quitter', 'Quit', 'QUIT', 'Escape', 'ESCAPE', 'ESC', 'esc', 'quiter', 'kiter', 'qiter', 'kit'],
+    'partition':['Partition', 'PART', 'partition', 'part', 'parti', 'partoche', 'Particion', 'Partission', 'PARTICION', 'PARTISSION'],
+    'compositeur':['compositeur', 'Compositeur', 'Composer', 'COMPOSER', 'Comp', 'COMP', 'conpositeur', 'conpo'],
+    'instrument':['Instrument', 'instrument', 'instrumant', 'instru', 'instrumen', 'instruman'],
+    'enregistrement':['Enregistrement', 'enregistrement', 'RECORD', 'record', 'REC', 'Rec', 'Record', 'Enreg', 'enregistremant'],
+    'editeur':['Editeur', 'editeur', 'edit', 'EDIT', 'editeure', 'editere', 'editeurre', 'editer'],
+    'oui':['Oui', 'oui', 'Yes', 'Si', 'Y', 'OUI', 'ui', 'UI'],
+    'non':['Non', 'non', 'No', 'N', 'NON', 'NEIN', 'nein',],
+    'modifier':['Modifier', 'modifier', 'mod', 'MOD', 'change', 'modif', 'modife', 'modifer'],
+    'annuler':['Annuler', 'annuler', 'Cancel', 'cancel', 'CANCEL', 'Annul', 'anuler', 'Anuler'],
+    'format':['Format', 'format', 'forme', 'form', 'size', 'Size', 'taille', 'TAILLE', 'Taille']
 
 }
 
-
-#HUGOOOOOOOOOOO
-
-
-#Non
 
